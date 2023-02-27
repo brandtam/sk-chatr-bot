10b4dab3bdf85609484cf73f325018f8fedd2153
Brandt Milczewski <brandt@shopdevs.com>
2023-02-27 14:31:32 -0800

    feat: Add homepage form with prompt and answer sections. feat: Add API rout to reach out to OpenAI and stream back the answer.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,74 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import type { CreateCompletionResponse } from 'openai'
+	import { SSE } from "sse.js";
+
+	let context = ''
+	let loading = false
+	let error: boolean | null = null
+	let answer = ''
+
+	const handleSubmit = async () => {
+		loading = true
+		error = null
+		answer = ''
+
+		const eventSource = new SSE('/api/answer', {
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			payload: JSON.stringify({ context })
+		})
+
+		context = ''
+
+		eventSource.addEventListener('error', (e) => {
+			error = true
+			loading = false
+			alert('Something went wrong')
+		})
+
+		eventSource.addEventListener('message', (e) => {
+			try {
+				loading = false
+
+				if (e.data === '[DONE]') {
+					eventSource.close()
+					return
+				}
+
+				const completionResponse: CreateCompletionResponse = JSON.parse(e.data)
+				const [{text}] = completionResponse.choices
+				answer = (answer ?? '' ) + text
+
+			} catch (err) {
+				error = true
+				loading = false
+				console.log(err)
+				alert('Something went wrong')}
+		})
+
+		eventSource.stream()
+
+	}
+</script>
+
+<div class="flex flex-col items-center py-10">
+	<div class="flex flex-col items-center prose card w-96 bg-black shadow-xl text-center">
+		<h1 class="text-purple-600 pt-4">Ask Me Anything</h1>
+		<form on:submit|preventDefault={() => handleSubmit()}>
+			<label class="text-yellow-500" for="context">What are you wondering?</label>
+			<textarea class="bg-slate-200 w-3/4" name="context" id="context" rows="5" bind:value={context}></textarea>
+			<button class="btn w-3/4 bg-purple-600 hover:bg-blue-600" type="submit">Ask</button>
+		</form>
+		<div class="w-3/4">
+			<h2>Answer</h2>
+			{#if loading}
+				<p class="text-yellow-500">Loading...</p>
+			{:else if error}
+				<p class="text-yellow-500">Something went wrong</p>
+			{:else}
+				<p class="text-yellow-500 text-left">{answer}</p>
+			{/if}
+		</div>
+	</div>
+</div>
---
+++ src/routes/api/answer/+server.ts
@@ -0,0 +1,67 @@
+import { OPENAI_KEY } from '$env/static/private';
+import { oneLine, stripIndent } from 'common-tags';
+import type { RequestHandler } from './$types';
+import type { CreateCompletionRequest } from 'openai';
+import { error } from '@sveltejs/kit';
+
+export const POST: RequestHandler = async ({ request }) => {
+	try {
+		if (!OPENAI_KEY) {
+			throw new Error('No OPENAI_KEY');
+		}
+
+		const requestData = await request.json();
+
+		if (!requestData) {
+			throw new Error('No requestData');
+		}
+
+		const { context } = requestData;
+
+		if (!context) {
+			throw new Error('No context');
+		}
+
+		const prompt = stripIndent`
+		${oneLine`
+		You are a grumpy old man who hates explaining things be does so anyways. Provide an answer or explanation to the following question:
+		`}
+
+		Context: """${context.trim()}"""
+
+		Answer:
+		`;
+
+		const completionOpts: CreateCompletionRequest = {
+			model: 'text-davinci-003',
+			prompt,
+			max_tokens: 100,
+			temperature: 0.9,
+			stream: true
+		};
+
+		const response = await fetch('https://api.openai.com/v1/completions', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${OPENAI_KEY}`
+			},
+			body: JSON.stringify(completionOpts)
+		});
+
+		if (!response.ok) {
+			const error = await response.json();
+			console.log(error);
+			throw new Error('Bad response from OpenAI', error);
+		}
+
+		return new Response(response.body, {
+			headers: {
+				'Content-Type': 'text/event-stream'
+			}
+		});
+	} catch (err) {
+		console.error(err);
+		throw error(500, 'Error in answer route');
+	}
+};
